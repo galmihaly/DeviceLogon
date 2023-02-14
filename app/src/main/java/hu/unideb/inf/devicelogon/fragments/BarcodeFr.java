7453801eb823f5a1beddc99d0ada5bfe5f0737a3
galmihaly <galmihaly118@gmail.com>
2023-02-14 15:38:17 +0100

    kjfffgfg

---
+++ app/src/main/java/hu/unideb/inf/devicelogon/fragments/BarcodeFr.java
@@ -0,0 +1,26 @@
+package hu.unideb.inf.devicelogon.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hu.unideb.inf.devicelogon.R;
+import hu.unideb.inf.devicelogon.fragments.BaseFragment;
+
+public class BarcodeFr extends BaseFragment {
+
+    private View view;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        view = inflater.inflate(R.layout.fragment_barcode, container, false);
+
+        return view;
+    }
+}
